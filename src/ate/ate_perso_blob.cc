9476ca51eda5e714c00d2690dc9a5d9fe0362b65
Miguel Osorio <miguelosorio@google.com>
2025-05-22 21:40:24 -0700

    [wip] perso blob parser
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ src/ate/ate_perso_blob.cc
@@ -0,0 +1,5 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/manuf/base/perso_tlv_data.h"
